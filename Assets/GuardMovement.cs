15e543499e1f97bf6832db33f2ccff3e3820450f
João Vitor Dias <joao.dias.santos223@gmail.com>
2022-05-19 07:26:51 -0300

    feat(): make vision collider rotate with guard
    Signed-off-by: João Vitor Dias <joao.dias.santos223@gmail.com>

--- Assets/GuardMovement.cs
+++ Assets/GuardMovement.cs
@@ -2,6 +2,8 @@ using System;
 using static System.Math;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class GuardMovement : MonoBehaviour
@@ -9,6 +11,7 @@ public class GuardMovement : MonoBehaviour
     public float movementSpeed = 2f;
     public float maxVelocity = 5f;
     public Vector2 movement;
+    public Vector2 direction;
     
     public Rigidbody2D rb2D;
     public Animator animator;
@@ -29,6 +32,7 @@ public class GuardMovement : MonoBehaviour
     [SerializeField] private int waypointIndex = 0;
     [SerializeField] private Transform visionCollider;
 
+
     private void Start ()
     {
         waypoints = new List<Transform>();
@@ -36,14 +40,21 @@ public class GuardMovement : MonoBehaviour
         {
             waypoints.Add(waypoint);
         }
-        
-        movement = waypoints[0].position;
+
+        movement = Vector2.zero;
+        direction = Vector2.zero;
     }
 
     private void Update()
     {
-        animator.SetFloat(ANIM_HORIZONTAL_PARAM, (float)Clamp(movement.x - transform.position.x, -1.0, 1.0));
-        animator.SetFloat(ANIM_VERTICAL_PARAM, (float)Clamp(movement.y - transform.position.y, -1.0, 1.0));
+        Vector3 position = transform.position;
+        direction.x = (float) Clamp(movement.x - position.x, -1.0, 1.0);
+        direction.y = (float) Clamp(movement.y - position.y, -1.0, 1.0);
+
+        RotateColliderBasedOnDirection();
+
+        animator.SetFloat(ANIM_HORIZONTAL_PARAM, direction.x);
+        animator.SetFloat(ANIM_VERTICAL_PARAM, direction.y);
         animator.SetFloat(ANIM_SPEED_PARAM, movement.sqrMagnitude);
     }
 
@@ -112,5 +123,85 @@ public class GuardMovement : MonoBehaviour
         Vector2 newPosition = Vector2.MoveTowards(transform.position, waypoint, Time.deltaTime * movementSpeed);
         rb2D.MovePosition(newPosition);
         rb2D.velocity = Vector2.ClampMagnitude(rb2D.velocity, maxVelocity);
+    }
+
+    private void RotateColliderBasedOnDirection()
+    {
+        Vector3 rotation = visionCollider.rotation.eulerAngles;
+        visionCollider.rotation = Quaternion.Euler(rotation.x, rotation.y, GetZetaValueBasedOnAnimationState());
+    }
+
+    private float GetZetaValueBasedOnAnimationClip()
+    {
+        float z = 0;
+        AnimatorClipInfo[] currentAnim = animator.GetCurrentAnimatorClipInfo(0);
+
+        Debug.Log(currentAnim[0].clip.name);
+
+        z = currentAnim[0].clip.name switch
+        {
+            "Walk_Down" => 0,
+            "Walk_Up" => 180,
+            "Walk_Right" => 90,
+            "Walk_Left" => 270,
+            _ => z
+        };
+
+        return z;
+    }
+
+    private float GetZetaValueBasedOnAnimationState()
+    {
+        float z = 0;
+        AnimatorController animatorController = GetAnimatorController();
+
+        if (animatorController == null)
+        {
+            Debug.Log("Animator controller is Null");
+            return z;
+        }
+        AnimatorStateMachine animatorStateMachine = animatorController.layers[0].stateMachine;
+        AnimatorState movementState = animatorStateMachine.states[1].state;
+        Motion blendTree = (BlendTree) movementState.motion;
+        blendTree.
+
+        if (movementState.name != "Movement")
+        {
+            Debug.Log("WRONG STATE");
+            return z;
+        }
+
+        Debug.LogFormat("Current state: {0}", movementState.name);
+        Debug.LogFormat("Current motion: {0}", blendTree.name);
+
+        z = blendTree.name switch
+        {
+            "Walk_Down" => 0,
+            "Walk_Up" => 180,
+            "Walk_Right" => 90,
+            "Walk_Left" => 270,
+            _ => z
+        };
+
+        return z;
+    }
+
+    [CanBeNull]
+    private AnimatorController GetAnimatorController()
+    {
+        RuntimeAnimatorController runtimeController = animator.runtimeAnimatorController;
+        if (runtimeController == null)
+        {
+            Debug.LogErrorFormat("RuntimeAnimatorController must not be null.");
+            return null;
+        }
+        AnimatorController assetAnimatorController =UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEditor.Animations.AnimatorController>(UnityEditor.AssetDatabase.GetAssetPath(runtimeController));
+        if (assetAnimatorController == null)
+        {
+            Debug.LogErrorFormat("AnimatorController must not be null.");
+            return null;
+        }
+
+        return assetAnimatorController;
     }
 }
